f26b15db03401f1eaf82f4707276238318e8dc08
freeman-tmc <freeman.tomic84@gmail.com>
2018-02-03 21:24:53 +0100

    promenjen na engleski

--- dvocifren.js
+++ dvocifren.js
@@ -2,13 +2,13 @@ var n = 36;
 
 if (typeof n == "number") {
     if (n > 9 && n < 100) {
-        var jedinice = n % 10;
-        var desetice = (n - jedinice) / 10;
-        var broj = jedinice*10 + desetice;
-        console.log(broj);
+        var units = n % 10;
+        var tenth = (n - units) / 10;
+        var number = units*10 + tenth;
+        console.log(number);
     } else {
-        console.log("greska");
+        console.log("Error!");
     }
 } else {
-    console.log("greska");
+    console.log("Error!");
 }
