e25acf5fb5c60d96e867a1b11daabfc3568b2702
BIT student <classroom@bgit.rs>
2018-02-05 17:52:55 +0100

    arrays file added

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var months = ["January", "February", "March", "April", "May", "June", "July"];
+
+console.log(months[0], months[5], months[4]);
+
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+console.log(days[6]);
+
+var myArray = [2, -4, 5, -2, -11];
+
+if (myArray[0] < 0) {
+    console.log(myArray[0]);
+}
+if (myArray[1] < 0) {
+    console.log(myArray[1]);
+}
+if (myArray[2] < 0) {
+    console.log(myArray[2]);
+}
+if (myArray[3] < 0) {
+    console.log(myArray[3]);
+}
+if (myArray[4] < 0) {
+    console.log(myArray[4]);
+}
+
+
+
+
+
+       
+    
